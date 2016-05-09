b7c9241fdb0e79e21498c7f7f415f3fd8e07a232
Zhang, Philip (China - Chengdu) <pzhang@aaxischina.com>
2016-05-09 18:35:00 +0800

    automatic the gae script

--- naf/go.sh
+++ naf/go.sh
@@ -1,11 +1,11 @@
 # !/bin/bash
 WEBINFDIR=/opt/resin/webapps/naf/WEB-INF
 echo "$WEBINFDIR/work"
-mv -rf "$WEBINFDIR/work" "$WEBINFDIR/workbackup"
-mv "$WEBINFDIR/web.xml" "$WEBINFDIR/local-web.xml"
-mv "$WEBINFDIR/gae-web.xml" "$WEBINFDIR/web.xml"
+mv -f "$WEBINFDIR/work" "$WEBINFDIR/workbackup"
+mv -f "$WEBINFDIR/web.xml" "$WEBINFDIR/local-web.xml"
+cp -f "$WEBINFDIR/gae-web.xml" "$WEBINFDIR/web.xml"
 appcfg.sh update .
-mv -f "$WEBINFDIR/local-web.xml" "$WEBINFDIR/web.xml"
+cp -f "$WEBINFDIR/local-web.xml" "$WEBINFDIR/web.xml"
 mv -f "$WEBINFDIR/workbackup" "$WEBINFDIR/work"
 
 
